10186217469dd49dd65931a431138870f5ace126
eliseoeric <eric.eliseo@gmail.com>
2017-12-29 15:12:47 -0500

    Add background image hook
    This commit remvoes the background image from the CSS, and instead adds
    it as an `img` element via the `login_header` hook.

--- src/LoginPage.php
+++ src/LoginPage.php
@@ -3,13 +3,38 @@
 namespace Gearhead\Login;
 
 class LoginPage {
+
+	protected $background_image;
+	protected $path;
+
+	public function __construct() {
+		$this->path = dirname(dirname(__FILE__));
+	}
+
 	public function register_hooks() {
 		add_action('login_enqueue_scripts', [$this, 'enqueue_styles']);
+		add_action('login_header', [$this, 'add_background_image']);
 	}
 
 	public function enqueue_styles() {
-		$path = dirname(dirname(__FILE__));
-		$file = $path . '/Assets/css/login.css';
+		$file = $this->path . '/Assets/css/login.css';
 		wp_enqueue_style('gearhead-login', $file);
+	}
+
+	public function add_background_image() {
+		$classes = '';
+		$background_image = $this->background_image();
+		echo sprintf("<div class='background-image'><img src='%s' class='%s'></div>", $background_image, $classes);
+	}
+
+	public function background_image() {
+		$background_image = $this->background_image;
+
+		if (!$this->background_image) {
+			$background_image = 'forest.jpg';
+		}
+
+		return $this->path . '/Assets/images/' . $background_image;
+
 	}
 }
